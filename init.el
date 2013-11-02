af6a4e0961907572945a704b4930e88f03e49034
Carl Hu <carlhu@carlhu.com>
2013-11-02 03:07:29 -0400

    Minor commit.

--- init.el
+++ init.el
@@ -107,10 +107,290 @@ by Prelude.")
   (message "Loading personal configuration files in %s..." prelude-personal-dir)
   (mapc 'load (directory-files prelude-personal-dir 't "^[^#].*el$")))
 
+
+(setq visual-line-mode nil)
+(setq global-visual-line-mode nil)
+(setq org-indent-mode t)
+(setq org-startup-truncated nil)
+(setq auto-fill-mode -1)
+(setq-default fill-column 99999)
+(setq fill-column 99999)
+(setq org-startup-indented t)
+(setq org-confirm-babel-evaluate nil)
+(setq org-yank-adjusted-subtrees t) ; advanced cut and paste behavior for orgmode points.
+(setq org-yank-folded-subtrees t) ; advanced cut and paste behavior for orgmode points.
+(setq org-hide-leading-stars t)
+(setq org-odd-level-only nil) 
+;; configure org
+(setq org-insert-heading-respect-content nil)
+;; (setq org-M-RET-may-split-line t)
+(setq org-M-RET-may-split-line '((item) (default . t)))
+;(setq org-M-RET-may-split-line t)
+(setq org-special-ctrl-a/e t)
+(setq org-return-follows-link nil)
+;(setq org-use-speed-commands t)
+(setq org-startup-align-all-tables nil)
+(setq org-tags-column 0) 
+;; (setq org-archive-location (concat org-directory "archive/%s_archive::"))
+;; (setq org-agenda-remove-tags t)
+;;(setq org-treat-S-cursor-todo-selection-as-state-change t)
+(setq org-log-into-drawer nil)
+(setq org-link-frame-setup '((vm . vm-visit-folder-other-frame)
+                             (gnus . org-gnus-no-new-news)
+                             (file . find-file-other-window)
+                             (wl . wl-other-frame))) 
+(setq org-use-speed-commands t)
+(setq org-speed-commands-user nil)
+(add-to-list 'org-speed-commands-user
+             '("n" ded/org-show-next-heading-tidily))
+(add-to-list 'org-speed-commands-user
+             '("p" ded/org-show-previous-heading-tidily))
+(setq org-completion-use-ido t)
+(setq ido-auto-merge-delay-time 1)
+(setq ido-everywhere t)
+(ido-mode 'both)
+(global-auto-revert-mode t)
+(global-set-key (kbd "\C-m") 'newline-and-indent)
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 4)
+;; prompt simplification
+(fset 'yes-or-no-p 'y-or-n-p)
+(setq confirm-nonexistent-file-or-buffer nil)
+(setq ido-create-new-buffer 'always)
+(setq inhibit-startup-message t
+      inhibit-startup-echo-area-message t)
+(setq kill-buffer-query-functions
+      (remq 'process-kill-buffer-query-function
+            kill-buffer-query-functions))
+(require 'paren)
+(set-face-background 'show-paren-match-face (face-background 'default))
+(set-face-foreground 'show-paren-match-face "#def")
+(set-face-attribute 'show-paren-match-face nil :weight 'extra-bold)
+(show-paren-mode 1)
+(setq show-paren-delay 0)
+(prefer-coding-system 'utf-8)
+(defadvice save-buffers-kill-emacs (around no-y-or-n activate)
+  (flet ((yes-or-no-p (&rest args) t)
+         (y-or-n-p (&rest args) t))
+    ad-do-it))
+(global-set-key [(control next)] 'next-buffer) 
+(global-set-key [(control prior)] 'previous-buffer)
+(define-key global-map (kbd "M-c") 'quick-copy-line)
+;(define-key global-map (kbd "<f4>") 'magit-status)
+; (global-set-key (kbd "\C-x\C-e") 'org-export-as-html)
+(define-key global-map (kbd "C-/") 'hippie-expand) 
+(define-key minibuffer-local-map (kbd "C-/") 'hippie-expand)
+(define-key global-map (kbd "C-=") 'kill-current-buffer-and-frame)
+(define-key global-map (kbd "M-<f3>") 'switch-to-minus-todo)
+(define-key global-map (kbd "<f3>") 'switch-to-personal-todo)
+(define-key global-map (kbd "C-<f3>") 'switch-to-init-el)
+;; (define-key global-map (kbd "<f4>") 'org-archive-to-archive-sibling)
+(define-key global-map (kbd "<f4>") 'org-archive-subtree)
+
+; (define-key global-map (kbd "C-<f4>") 'ido-kill-buffer)
+(define-key global-map (kbd "C-<escape>") 'switch-to-blank)
+
+;(global-set-key (kbd "C-x C-f") 'lusty-file-explorer)
+;(global-set-key (kbd "C-x f") 'find-file)
+(global-set-key (kbd "<f2>") 'my-anything-occur)
+(global-set-key (kbd "<f1>") 'my-anything-switcher)
+(global-set-key (kbd "C-M-<up>") 'my-goto-first-org-item)
+;; (global-set-key (kbd "M-t") 'my-anything-switcher-at-point)
+;;(global-set-key (kbd "M-t") 'ido-switch-buffer)
+(global-set-key (kbd "C-c n") 'switch-to-notes)
+(global-set-key (kbd "C-S-f") 'my-anything-grep)
+(global-set-key (kbd "C-<f2>") 'my-anything-grep)
+(global-set-key (kbd "M-<f6>") 'my-anything-grep-sql)
+(global-set-key (kbd "<f7>") 'my-anything-grep-def)
+(global-set-key (kbd "S-<f7>") 'my-anything-grep)
+(global-set-key (kbd "C-<f7>") 'my-anything-grep-ref)
+(global-set-key (kbd "M-C-<f2>") 'anything-grep)
+(global-set-key [M-up] 'move-text-up)
+(global-set-key [M-down] 'move-text-down)
+(global-set-key (kbd "C-S-o")  'occur)
+(global-set-key (kbd "C-x b") 'switch-to-buffer)
+(define-key global-map (kbd "C-g") 'keyboard-escape-quit)
+(global-set-key (kbd "C-c C-8") 'org-ctrl-c-star)
+;;; esc quits
+(define-key global-map (kbd "<escape>") 'keyboard-escape-quit)
+(define-key minibuffer-local-map [escape] 'keyboard-escape-quit)
+(define-key minibuffer-local-ns-map [escape] 'keyboard-escape-quit)
+(define-key minibuffer-local-completion-map [escape] 'keyboard-escape-quit)
+(define-key minibuffer-local-must-match-map [escape] 'keyboard-escape-quit)
+(define-key minibuffer-local-isearch-map [escape] 'keyboard-escape-quit)
+
+(define-key global-map (kbd "<f9>") 'split-window-horizontally)
+
+;; Tell orgmode table to consider extra chars to be numbers.
+(setq org-table-number-regexp "^\\([<>]?[-+^.0-]*[0-9-\?][-+^.0-9eE dDx()%:xX,]*\\|\\(0[xX]\\)[0-9a-fA-F]+\\)$")
+
+(global-set-key (kbd "C-x <right>") 'windmove-right)
+(global-set-key (kbd "C-x <left>") 'windmove-left)
+(global-set-key (kbd "C-o") 'other-window)
+(global-set-key (kbd "C-M-<right>") 'windmove-right)
+(global-set-key (kbd "C-M-<left>") 'windmove-left)
+
+(global-set-key (kbd "M-C-w") 'clipboard-kill-ring-save)
+
+(setq my-org-heading-finder
+   [?\C-u ?\C-c ?\C-w])
+
+(define-key global-map (kbd "<f8>") 'my-org-heading-finder)
+; (define-key global-map (kbd "C-<f1>") 'other-window)
+
+
+(add-hook 'org-mode-hook
+          '(lambda ()
+             (define-key org-mode-map (kbd "C-<f5>") 'my-org2html)
+             (define-key org-mode-map (kbd "C-S-<f5>") 'my-org2html-open)
+             (define-key org-mode-map (kbd "C-c C-<f5>") 'my-pattern-export-orgmode)
+             (define-key org-mode-map (kbd "M-<f5>") 'my-import-orgmode)
+             (define-key org-mode-map (kbd "<f5>") 'my-blog-export)
+             (define-key org-mode-map (kbd "<f4>") 'org-archive-subtree)
+;             (define-key org-mode-map (kbd "<f7>") 'export-to-tex)
+             ;; (define-key org-mode-map (kbd "M-W") 'org-copy-special)
+             (define-key org-mode-map (kbd "M-W") 'my-copy-orgmode)             
+             (define-key org-mode-map (kbd "C-S-w") 'org-cut-special)
+             (define-key org-mode-map (kbd "C-c C-d") 'org-time-stamp)
+             (define-key org-mode-map (kbd "C-t") 'org-ctrl-c-ctrl-c)
+             ))
+
+;;; (define-key global-map (kbd "M-W") 'org-copy-special)
+
+
+(add-hook 'html-mode-hook
+          '(lambda ()
+             (define-key html-mode-map (kbd "<f5>") 'my-blog-export)))
+
+(add-hook 'css-mode-hook
+          '(lambda ()
+             (define-key css-mode-map (kbd "<f5>") 'my-blog-export)))
+
+(add-hook 'xml-mode-hook
+          '(lambda ()
+             (define-key xml-mode-map (kbd "<f5>") 'my-blog-export)))
+
+(global-set-key [(control next)] 'next-buffer) 
+(global-set-key [(control prior)] 'previous-buffer)
+(define-key global-map (kbd "M-c") 'quick-copy-line)
+;(define-key global-map (kbd "<f4>") 'magit-status)
+; (global-set-key (kbd "\C-x\C-e") 'org-export-as-html)
+(define-key global-map (kbd "C-/") 'hippie-expand) 
+(define-key minibuffer-local-map (kbd "C-/") 'hippie-expand)
+(define-key global-map (kbd "C-=") 'kill-current-buffer-and-frame)
+(define-key global-map (kbd "M-<f3>") 'switch-to-minus-todo)
+(define-key global-map (kbd "<f3>") 'switch-to-personal-todo)
+(define-key global-map (kbd "C-<f3>") 'switch-to-init-el)
+;; (define-key global-map (kbd "<f4>") 'org-archive-to-archive-sibling)
+(define-key global-map (kbd "<f4>") 'org-archive-subtree)
+
+; (define-key global-map (kbd "C-<f4>") 'ido-kill-buffer)
+(define-key global-map (kbd "C-<escape>") 'switch-to-blank)
+
+;(global-set-key (kbd "C-x C-f") 'lusty-file-explorer)
+;(global-set-key (kbd "C-x f") 'find-file)
+(global-set-key (kbd "<f2>") 'my-anything-occur)
+(global-set-key (kbd "<f1>") 'my-anything-switcher)
+(global-set-key (kbd "C-M-<up>") 'my-goto-first-org-item)
+;; (global-set-key (kbd "M-t") 'my-anything-switcher-at-point)
+;;(global-set-key (kbd "M-t") 'ido-switch-buffer)
+(global-set-key (kbd "C-c n") 'switch-to-notes)
+(global-set-key (kbd "C-S-f") 'my-anything-grep)
+(global-set-key (kbd "C-<f2>") 'my-anything-grep)
+(global-set-key (kbd "M-<f6>") 'my-anything-grep-sql)
+(global-set-key (kbd "<f7>") 'my-anything-grep-def)
+(global-set-key (kbd "S-<f7>") 'my-anything-grep)
+(global-set-key (kbd "C-<f7>") 'my-anything-grep-ref)
+(global-set-key (kbd "M-C-<f2>") 'anything-grep)
+(global-set-key [M-up] 'move-text-up)
+(global-set-key [M-down] 'move-text-down)
+(global-set-key (kbd "C-S-o")  'occur)
+(global-set-key (kbd "C-x b") 'switch-to-buffer)
+(define-key global-map (kbd "C-g") 'keyboard-escape-quit)
+(global-set-key (kbd "C-c C-8") 'org-ctrl-c-star)
+;;; esc quits
+(define-key global-map (kbd "<escape>") 'keyboard-escape-quit)
+(define-key minibuffer-local-map [escape] 'keyboard-escape-quit)
+(define-key minibuffer-local-ns-map [escape] 'keyboard-escape-quit)
+(define-key minibuffer-local-completion-map [escape] 'keyboard-escape-quit)
+(define-key minibuffer-local-must-match-map [escape] 'keyboard-escape-quit)
+(define-key minibuffer-local-isearch-map [escape] 'keyboard-escape-quit)
+
+(define-key global-map (kbd "<f9>") 'split-window-horizontally)
+
+;; Tell orgmode table to consider extra chars to be numbers.
+(setq org-table-number-regexp "^\\([<>]?[-+^.0-]*[0-9-\?][-+^.0-9eE dDx()%:xX,]*\\|\\(0[xX]\\)[0-9a-fA-F]+\\)$")
+
+(global-set-key (kbd "C-x <right>") 'windmove-right)
+(global-set-key (kbd "C-x <left>") 'windmove-left)
+(global-set-key (kbd "C-o") 'other-window)
+(global-set-key (kbd "C-M-<right>") 'windmove-right)
+(global-set-key (kbd "C-M-<left>") 'windmove-left)
+
+(global-set-key (kbd "M-C-w") 'clipboard-kill-ring-save)
+
+(setq my-org-heading-finder
+   [?\C-u ?\C-c ?\C-w])
+
+(define-key global-map (kbd "<f8>") 'my-org-heading-finder)
+; (define-key global-map (kbd "C-<f1>") 'other-window)
+
+
+(add-hook 'org-mode-hook
+          '(lambda ()
+             (define-key org-mode-map (kbd "C-<f5>") 'my-org2html)
+             (define-key org-mode-map (kbd "C-S-<f5>") 'my-org2html-open)
+             (define-key org-mode-map (kbd "C-c C-<f5>") 'my-pattern-export-orgmode)
+             (define-key org-mode-map (kbd "M-<f5>") 'my-import-orgmode)
+             (define-key org-mode-map (kbd "<f5>") 'my-blog-export)
+             (define-key org-mode-map (kbd "<f4>") 'org-archive-subtree)
+;             (define-key org-mode-map (kbd "<f7>") 'export-to-tex)
+             ;; (define-key org-mode-map (kbd "M-W") 'org-copy-special)
+             (define-key org-mode-map (kbd "M-W") 'my-copy-orgmode)             
+             (define-key org-mode-map (kbd "C-S-w") 'org-cut-special)
+             (define-key org-mode-map (kbd "C-c C-d") 'org-time-stamp)
+             (define-key org-mode-map (kbd "C-t") 'org-ctrl-c-ctrl-c)
+             ))
+
+;;; (define-key global-map (kbd "M-W") 'org-copy-special)
+
+
+(add-hook 'html-mode-hook
+          '(lambda ()
+             (define-key html-mode-map (kbd "<f5>") 'my-blog-export)))
+
+(add-hook 'css-mode-hook
+          '(lambda ()
+             (define-key css-mode-map (kbd "<f5>") 'my-blog-export)))
+
+(add-hook 'xml-mode-hook
+          '(lambda ()
+             (define-key xml-mode-map (kbd "<f5>") 'my-blog-export)))
+
+(add-hook 'org-mode-hook
+      (lambda ()
+        (if window-system
+            nil
+          (progn
+            (define-key org-mode-map "\C-\M-j" 'org-meta-return)
+            (define-key org-mode-map "\C-j" 'org-insert-heading-respect-content)))))
+
+(message "My init file is completely loaded.")
+
+
+
+(message "My init file is completely loaded.")
+
+
+(require 'color-theme)
+(color-theme-solarized-dark)
+(enable-theme 'zenburn)
+
 (message "Prelude is ready to do thy bidding, Master %s!" current-user)
 
 (prelude-eval-after-init
  ;; greet the use with some useful tip
  (run-at-time 5 nil 'prelude-tip-of-the-day))
+
 
 ;;; init.el ends here
